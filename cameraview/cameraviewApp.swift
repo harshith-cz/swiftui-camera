45eaaaa961907593f5c38117b879b6c647622f64
harshith-cz <harshith@codezyng.com>
2025-04-18 13:54:56 +0530

    Initial Commit

---
+++ cameraview/cameraviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraviewApp.swift
+//  cameraview
+//
+//  Created by Harshith on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct cameraviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
